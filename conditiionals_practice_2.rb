030360d9dc1e2aab1dcbddb0de21ea94d972b903
Alex Racinski <aracinski2@gmail.com>
2022-03-19 17:51:29 -0500

    complete problem 2

--- conditiionals_practice_2.rb
+++ conditiionals_practice_2.rb
@@ -1,7 +1,17 @@
 # 1
+# number = gets.chomp
+# if number.to_i == 10
+#   p 0
+# else
+#   p -1
+# end 
+
+# 2
 number = gets.chomp
-if number.to_i == 10
-  p 0
-else
+if number.to_i < 10
   p -1
-end 
+elsif number.to_i > 10
+  p 1
+else
+  p 0
+end
