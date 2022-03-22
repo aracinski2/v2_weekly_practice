6398c6ceb7590d5850ee2f2421225538b142f9da
Alex Racinski <aracinski2@gmail.com>
2022-03-22 18:30:33 -0500

    complete strings practice problem 4

--- strings_practice_problems_2.rb
+++ strings_practice_problems_2.rb
@@ -9,10 +9,16 @@
 # p "#{fname} #{lname}"
 
 # 3
-p "Please input a word"
-word = gets.chomp 
-if word == "marco"
-  p "polo"
-else
-  p "Incorrect"
-end
+# p "Please input a word"
+# word = gets.chomp 
+# if word == "marco"
+#   p "polo"
+# else
+#   p "Incorrect"
+# end
+
+# 4
+c1 = "green"
+c2 = "red"
+c3 = "blue"
+p "The " + c1 + " car looks " + c2 + " in this " + c3 + " sky."
