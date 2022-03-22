f2ce4154c03d4a55e45528bb66f5621764d22d9f
Alex Racinski <aracinski2@gmail.com>
2022-03-22 18:27:12 -0500

    complete strings practice problem 3

--- strings_practice_problems_2.rb
+++ strings_practice_problems_2.rb
@@ -4,6 +4,15 @@
 # p fname + " " + lname
 
 # 2
-fname = "jay"
-lname = "fray"
-p "#{fname} #{lname}"
+# fname = "jay"
+# lname = "fray"
+# p "#{fname} #{lname}"
+
+# 3
+p "Please input a word"
+word = gets.chomp 
+if word == "marco"
+  p "polo"
+else
+  p "Incorrect"
+end
