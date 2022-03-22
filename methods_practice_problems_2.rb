3c1c66beadbc6fa47e70e472ce12be1090b13440
Alex Racinski <aracinski2@gmail.com>
2022-03-22 18:49:02 -0500

    complete methods practice problem 4

--- methods_practice_problems_2.rb
+++ methods_practice_problems_2.rb
@@ -11,7 +11,12 @@
 # end
 
 # 3
-p "Please enter two numbers"
-n1 = gets.chomp
-n2 = gets.chomp
-p n1.to_i + n2.to_i
+# p "Please enter two numbers"
+# n1 = gets.chomp
+# n2 = gets.chomp
+# p n1.to_i + n2.to_i
+
+# 4
+p "Please enter a word"
+word = gets.chomp
+p word.reverse
