5f9a092b552b589292b99ac806889637fc72175c
Alex Racinski <aracinski2@gmail.com>
2022-03-22 18:47:47 -0500

    complete methods practice problem 3

--- methods_practice_problems_2.rb
+++ methods_practice_problems_2.rb
@@ -4,8 +4,14 @@
 # p word.upcase
 
 # 2
-p "Please enter a number"
-number = gets.chomp
-if number.to_i > 100
-  p "That's a big number"
-end
+# p "Please enter a number"
+# number = gets.chomp
+# if number.to_i > 100
+#   p "That's a big number"
+# end
+
+# 3
+p "Please enter two numbers"
+n1 = gets.chomp
+n2 = gets.chomp
+p n1.to_i + n2.to_i
