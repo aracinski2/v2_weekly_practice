5d24080bd25fbd3ad0239280c327eab70d4eea4c
Alex Racinski <aracinski2@gmail.com>
2022-03-23 18:28:19 -0500

    complete array practice problem 3

--- arrays_practice_problems_2.rb
+++ arrays_practice_problems_2.rb
@@ -5,6 +5,14 @@
 # p array
 
 # 2
-array = ["a", "c", "s", "p"]
-array[1] = 4
-p array
+# array = ["a", "c", "s", "p"]
+# array[1] = 4
+# p array
+
+# 3
+array = [1, 2, 3, 4, 5]
+index = 0
+while index < array.length
+  p array[index]
+  index += 1
+end
