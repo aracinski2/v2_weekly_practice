a667d6aad43c620dbaa1468688ef30aef06435f9
Alex Racinski <aracinski2@gmail.com>
2022-03-23 18:27:05 -0500

    complete array practice problem 2

--- arrays_practice_problems_2.rb
+++ arrays_practice_problems_2.rb
@@ -1,5 +1,10 @@
 # 1
-array = ["bar", "far", "car"]
-array << "jar"
-array << "har"
+# array = ["bar", "far", "car"]
+# array << "jar"
+# array << "har"
+# p array
+
+# 2
+array = ["a", "c", "s", "p"]
+array[1] = 4
 p array
