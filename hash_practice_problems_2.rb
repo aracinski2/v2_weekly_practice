0bfa757260799b9b72bbda1b7715689606dcccdb
Alex Racinski <aracinski2@gmail.com>
2022-03-23 19:06:40 -0500

    complete hash practice problem 2

--- hash_practice_problems_2.rb
+++ hash_practice_problems_2.rb
@@ -1,6 +1,15 @@
 # 1
-person = {"fname" => "Billy", "lname" => "Bob", "email" => "billy@bob"}
-p person
-p person["fname"]
-p person["lname"]
-p person["email"]
+# person = {"fname" => "Billy", "lname" => "Bob", "email" => "billy@bob"}
+# p person
+# p person["fname"]
+# p person["lname"]
+# p person["email"]
+
+# 2
+people = [
+  {"fname" => "Jery", "lname" => "Summer"}, 
+  {"fname" => "Molly", "lname" => "Kav"}, 
+  {"fname" => "Pete", "lname" => "Mac"}
+]
+p people[0]["fname"]
+p people[0]["lname"]
