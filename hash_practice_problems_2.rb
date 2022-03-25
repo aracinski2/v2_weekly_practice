023e2916e7456b63d76d73c88eef015f16807e64
Alex Racinski <aracinski2@gmail.com>
2022-03-24 19:35:17 -0500

    complete hash practice problem 4

--- hash_practice_problems_2.rb
+++ hash_practice_problems_2.rb
@@ -15,6 +15,18 @@
 # p people[0]["lname"]
 
 # 3
-menu = {"hotdog" => "3.00", "burger" => "4.50", "fires" => "2.00"}
-menu["soda"] = "1.50"
-p menu
+# menu = {"hotdog" => "3.00", "burger" => "4.50", "fires" => "2.00"}
+# menu["soda"] = "1.50"
+# p menu
+
+# 4
+book = {
+  "title" => "Harry Potter",
+  "author" => "JK Rowling",
+  "# of pages" => "1023",
+  "language" => "english"
+}
+p book["title"]
+p book["author"]
+p book["# of pages"]
+p book["language"]
