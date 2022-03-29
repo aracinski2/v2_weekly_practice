a2ca83fdb9df10582fa75fb12341b745b79bacd2
Alex Racinski <aracinski2@gmail.com>
2022-03-29 17:47:23 -0500

    complete arrays map practice using while loop problem 2

--- arrays_map_practice_problems_2.rb
+++ arrays_map_practice_problems_2.rb
@@ -1,9 +1,19 @@
 # 1
-array_1 = [1, 2, 3]
+# array_1 = [1, 2, 3]
+# index = 0
+# new_array = []
+# while index < array_1.length
+#   new_array << array_1[index] * 3
+#   index += 1
+# end
+# p new_array
+
+# 2
+strings = ["hello", "goodbye"]
 index = 0
-new_array = []
-while index < array_1.length
-  new_array << array_1[index] * 3
+uppercase_string = []
+while index < strings.length
+  uppercase_string << strings[index].upcase
   index += 1
 end
-p new_array
+p uppercase_string
